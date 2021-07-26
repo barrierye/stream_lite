b2ed6f6a902e70c76883c1a9ca55b37cf1704a97
barrierye <barriery@qq.com>
2021-07-26 17:33:26 +0800

    add subtask service

---
+++ stream_lite/client/subtask_client.py
@@ -0,0 +1,30 @@
+#-*- coding:utf8 -*-
+# Copyright (c) 2021 barriery
+# Python release: 3.7.0
+# Create time: 2021-07-19
+import grpc
+import logging
+import pickle
+import inspect
+import time
+from typing import List, Dict
+
+from stream_lite.proto import job_manager_pb2, job_manager_pb2_grpc
+from stream_lite.proto import task_manager_pb2, task_manager_pb2_grpc
+from stream_lite.proto import subtask_pb2, subtask_pb2_grpc
+from stream_lite.proto import common_pb2
+from stream_lite.network import serializator
+from stream_lite.utils import util
+from .client_base import ClientBase
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class SubTaskClient(ClientBase):
+
+    def __init__(self):
+        super(SubTaskClient, self).__init__()
+
+    def _init_stub(self, channel):
+        return subtask_pb2_grpc.SubTaskServiceStub(channel)
+
