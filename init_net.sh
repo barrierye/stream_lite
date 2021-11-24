682d774c7f404626254ecc35013803bbfdf7ab10
barrierye <barriery@qq.com>
2021-11-24 16:04:10 +0800

    update code

--- init_net.sh
+++ init_net.sh
@@ -22,12 +22,12 @@ function set_latency() {
 
 if [ "$hostname" == "38" ]; then
     init_env
-    echo "give 192.168.105.39 200ms latency"
-    set_latency 39 192.168.105.39 200
-    echo "give 192.168.105.40 300ms latency"
-    set_latency 40 192.168.105.40 300
-    echo "give 192.168.105.83 100ms latency"
-    set_latency 83 192.168.105.83 100
+    echo "give 192.168.105.39 20ms latency"
+    set_latency 39 192.168.105.39 20
+    echo "give 192.168.105.40 30ms latency"
+    set_latency 40 192.168.105.40 30
+    echo "give 192.168.105.83 10ms latency"
+    set_latency 83 192.168.105.83 10
     echo "give 192.168.105.84 5000ms latency"
     set_latency 84 192.168.105.84 5000
 elif [ "$hostname" == "84" ]; then
@@ -46,8 +46,8 @@ elif [ "$hostname" == "39" ]; then
     set_latency 83 192.168.105.83 5000
 elif [ "$hostname" == "40" ]; then
     init_env
-    echo "give 192.168.105.83 100ms latency"
-    set_latency 83 192.168.105.83 100
+    echo "give 192.168.105.83 10ms latency"
+    set_latency 83 192.168.105.83 10
 else
     echo "error hostname: $hostname"
     exit 1
