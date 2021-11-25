92ed4f1d50fc992846bd1f8b2b8d8f30b2612957
barrierye <barriery@qq.com>
2021-11-26 01:33:07 +0800

    update code

--- expr/ex2/resources/task_source_files/SimpleSource.py
+++ expr/ex2/resources/task_source_files/SimpleSource.py
@@ -17,7 +17,7 @@ class SimpleSource(SourceOperatorBase):
         self.register_var("counter")
 
     def compute(self, inputs):
-        time.sleep(0.2)
+        time.sleep(0.08)
         if self.counter < len(self.lines):
             word = self.lines[self.counter].strip()
             self.counter += 1
