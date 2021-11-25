dabb64ae683c403b4fd118523ba5693aaa96055d
barrierye <barriery@qq.com>
2021-11-25 17:04:12 +0800

    update code

--- expr/ex2/resources/task_source_files/DetOp.py
+++ expr/ex2/resources/task_source_files/DetOp.py
@@ -6,12 +6,21 @@ import os
 import time
 from typing import List, Dict, Optional, Tuple
 
+from paddle_serving_app.local_predict import LocalPredictor
 from stream_lite import OperatorBase
 
-class SimpleSum(OperatorBase):
+class DetOp(OperatorBase):
+
+    def init(self, resource_path_dict):
+        self.predictor = LocalPredictor()
+        os.system("tar -xzvf {}".format(resource_path_dict["ocr_det_model"]))
+        self.load_model_config(resource_path_dict["ocr_det_model"])
+        with open(resource_path_dict["document-words.txt"]) as f:
+            self.lines = f.readlines()
+        self.counter = 0
 
     def __init__(self):
-        super(SimpleSum, self).__init__()
+        super(DetOp, self).__init__()
         self.counter = {}
         self.register_var("counter")
 
@@ -19,6 +28,6 @@ class SimpleSum(OperatorBase):
         if data not in self.counter:
             self.counter[data] = 0
         self.counter[data] += 1
-        time.sleep(0.1)
+        time.sleep(0.01)
         #  print((data, self.counter[data]))
         return (data, self.counter[data])
