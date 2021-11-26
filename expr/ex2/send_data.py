64b98133cf7750bffeeefd7a76df7fd361329ff6
barrierye <barriery@qq.com>
2021-11-26 13:22:01 +0800

    update code

--- expr/ex2/send_data.py
+++ expr/ex2/send_data.py
@@ -3,10 +3,16 @@
 # Python release: 3.7.0
 # Create time: 2021-11-26
 import requests
+from time import time
 
 with open("./resources/document-words.txt") as f:
-    for line in f:
+    for idx, line in enumerate(f):
         line = line.strip()
-        a = requests.get("http://192.168.105.84:8081/api/put/1/line")
-        print(a)
-        break
+        st = time()
+        while True:
+            a = requests.get(
+                    "http://192.168.105.84:8081/api/put/{}/{}".format(idx, line))
+            if a.status_code == 200:
+                break
+        et = time()
+        print("P[{}] latency: {}ms".format(et - st))
