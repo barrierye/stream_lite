84389eda899e356ee71c9a86b4053c0dc80919d3
barrierye <barriery@qq.com>
2021-11-26 13:32:13 +0800

    update code

---
+++ expr/ex2/resources/task_source_files/HttpSink.py
@@ -0,0 +1,20 @@
+#-*- coding:utf8 -*-
+# Copyright (c) 2021 barriery
+# Python release: 3.7.0
+# Create time: 2021-07-24
+import os
+import requests
+
+from stream_lite import SinkOperatorBase
+
+class HttpSink(SinkOperatorBase):
+
+    def compute(self, inputs):
+        #self.fout.write("{}\n".format(inputs))
+        #self.fout.flush()
+        while True:
+            a = requests.get(
+                    "http://192.168.105.38:8998/api/recv")
+            if a.status_code == 200:
+                break
+            time.sleep(0.01)
