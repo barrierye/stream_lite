7d25a7a7dee3aa73d31fbe5e4ff8c032727a958a
barrierye <barriery@qq.com>
2021-11-26 13:17:09 +0800

    update code

--- expr/ex2/send_data.py
+++ expr/ex2/send_data.py
@@ -7,6 +7,6 @@ import requests
 with open("./resources/document-words.txt") as f:
     for line in f:
         line = line.strip()
-        a = requests.get("http://127.0.0.1:8080/api/data/1/line")
+        a = requests.get("http://127.0.0.1:8080/api/put/1/line")
         print(a)
         break
