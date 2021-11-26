53f1a24254cfc5b3ef2cd8e42618becf8f3e73a6
barrierye <barriery@qq.com>
2021-11-26 12:55:42 +0800

    update code

---
+++ expr/ex2/resources/task_source_files/HttpSource.py
@@ -0,0 +1,43 @@
+#-*- coding:utf8 -*-
+# Copyright (c) 2021 barriery
+# Python release: 3.7.0
+# Create time: 2021-07-24
+import time
+import re
+from flask import Flask
+import queue
+import threading
+
+from stream_lite import SourceOperatorBase
+from stream_lite.utils import FinishJobError
+
+class HttpSource(SourceOperatorBase):
+
+    def init(self, resource_path_dict):
+        self.counter = 0
+        self.register_var("counter")
+        self.input_que = queue.Queue()
+        
+        def run(que, port):
+            self.app = Flask(__name__)
+
+            @app.route("/api/put/<int:line>/<str:data>")
+            def recv_data(line, data):
+                que.put((line, data))
+                return "ok\n"
+
+            app.run(debug=True, port=port)
+
+        self._thread = threading.Thread(
+                target=run,
+                args=(self.input_que, 18080))
+        self._thread.start()
+
+    def compute(self, inputs):
+        line, data = self.input_que.get()
+        if self.counter > line:
+            continue
+        else:
+            self.counter += 1
+            word = data.strip()
+            return word
---
+++ expr/ex2/send_data.py
@@ -0,0 +1,12 @@
+#-*- coding:utf8 -*-
+# Copyright (c) 2021 barriery
+# Python release: 3.7.0
+# Create time: 2021-11-26
+import requests
+
+with open("./resources/document-words.txt") as f:
+    for line in f:
+        line = line.strip()
+        a = resources.get("192.168.105.84:18080/api/data/1/line")
+        print(a)
+        break
