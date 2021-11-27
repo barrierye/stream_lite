8a9a42a492cc24ceba04017eeac8f6ea2efadad9
barrierye <barriery@qq.com>
2021-11-27 15:06:55 +0800

    update code

--- stream_lite/job_manager/checkpoint_helper.py
+++ stream_lite/job_manager/checkpoint_helper.py
@@ -253,7 +253,7 @@ class PrecopyAndMigrateHelper(PeriodicExecutorBase):
                                     jobid, cls_name, partition_idx), file_name)
                 endpoint = resource_manager_client.getTaskManagerEndpoint(
                         target_task_manager_locate)
-                cmd = "scp {} root@{}:{}".format(full_fn, endpoint, remote_full_fn)
+                cmd = "scp {} root@{}:{}".format(local_full_fn, endpoint, remote_full_fn)
                 print("run {}".format(cmd))
                 os.system(cmd)
 
